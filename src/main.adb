1e423036190d80bad833be102f077f5301e49b38
Nathan Gauër <nathan.gauer@lse.epita.fr>
2019-01-17 20:58:35 +0100

    debug: added debug method
    Signed-off-by: Nathan Gauër <nathan.gauer@lse.epita.fr>

--- src/main.adb
+++ src/main.adb
@@ -37,9 +37,11 @@ with Last_Chance_Handler;  pragma Unreferenced (Last_Chance_Handler);
 pragma Warnings (Off, "referenced");
 with Ada.Numerics.Elementary_Functions; use Ada.Numerics.Elementary_Functions;
 with Ada.Numerics.Float_Random; use Ada.Numerics.Float_Random;
+with Ada.Exceptions; use Ada.Exceptions;
 
 -- with STM32.Board;           use STM32.Board;
-with HAL.Bitmap;
+with HAL.Bitmap; use HAL.Bitmap;
+with LCD_Std_Out;
 -- with STM32.User_Button;     use STM32;
 
 with Ada.Real_Time; use Ada.Real_Time; -- for seconds
@@ -182,6 +184,16 @@ is
 			end if;
 		end loop;
 	end;
+
+	function min(A, B: Natural) return Natural is
+	begin
+		if A < B then
+			return A;
+		else
+			return B;
+		end if;
+	end min;
+
 begin
 	Renderer.Initialize;
 	Input_GameAccess.Initialize;
@@ -201,6 +213,27 @@ begin
 		Input_GameAccess.Poll;
 		Timer_GameAccess.Poll;
 
+		CollideParticles(game);
 		DrawFrame(game);
 	end loop;
+exception
+	when Error: others =>
+		declare
+			Msg : String := Exception_Information(Error);
+			Left : Natural := 1;
+			Right : Natural := 1;
+			Y : Natural := 0;
+		begin
+			while Right < Msg'Length loop
+				Right := min(Left + 30, Msg'Last);
+				LCD_Std_Out.Put(0, Y, Msg(Left .. Right));
+				Left := Right;
+				Y := Y + 10;
+			end loop;
+		end;
+		
+		LCD_Std_Out.Put(0, 30, Exception_Information(Error)(50 .. 75));
+		loop
+			null;
+		end loop;
 end Main;
