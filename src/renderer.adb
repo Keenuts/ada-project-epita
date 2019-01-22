9c43ea13ce69e278ccaf1d562adcbd7226b211a0
Nathan Gauër <nathan.gauer@lse.epita.fr>
2019-01-22 12:32:49 +0100

    renderer: sprite rendering in progress
    Signed-off-by: Nathan Gauër <nathan.gauer@lse.epita.fr>

--- src/renderer.adb
+++ src/renderer.adb
@@ -2,6 +2,47 @@ with STM32.Board;           use STM32.Board;
 
 package body Renderer is
 
+	-- sample a sprite
+	-- X and Y belongs to [0; 1] interval
+	-- Img must be a valid access
+	function Sample_Sprite(
+		Img : in Sprite_Access;
+		X : in Float;
+		Y : in Float
+	) return UInt8
+	is
+		Px : Natural := Natural(X * Float(SPRITE_SIZE));
+		Py : Natural := Natural(Y * Float(SPRITE_SIZE));
+		I : Natural := Py * SPRITE_SIZE + Px;
+	begin
+		return UInt8(Img(unsigned(I)));
+	end Sample_Sprite;
+
+	procedure Load_Sprite(
+		Source     : in Sprite_Access;
+		Indices    : in out Indexed_Bitmap;
+		Size 	   : in Positive)
+	is
+		Px, Py : Natural;
+		X, Y : Float;
+	begin
+		for I in Indices'Range loop
+			Px := I mod Size;
+			Py := I / Size;
+
+		 	X := Float(Px) / Float(Size);
+		 	Y := Float(Py) / Float(Size);
+
+			pragma Loop_Invariant (
+				I >= 0 and then I < Size * Size and then
+				Px >= 0 and then Px < Size and then
+				X >= 0.0 and then X <= 1.0 and then
+				Y >= 0.0 and then Y <= 1.0);
+
+			Indices(I) := Sample_Sprite(Source, X, Y);
+		 end loop;
+	end Load_Sprite;
+
 	procedure Initialize is
 	begin
 		--  Initialize LCD
@@ -9,6 +50,10 @@ package body Renderer is
 		Display.Initialize_Layer (1, ARGB_8888);
 		LCD_Std_Out.Set_Font (BMP_Fonts.Font8x8);
 		LCD_Std_Out.Current_Background_Color := BACKGROUND_COLOR;
+
+		-- Initialize sprites DMA-BUFs
+		Load_Sprite(SPRITE_ENEMY'Access,  ENEMY_Indices,  ENEMY_SPRITE_SIZE);
+		Load_Sprite(SPRITE_PLAYER'Access, PLAYER_Indices, PLAYER_SPRITE_SIZE);
 	end Initialize;
 
 	procedure Clear is
@@ -26,41 +71,65 @@ package body Renderer is
 	procedure DrawEnemy(id : in CellId) is
 		X : Integer;
 		Y : Integer;
+		framebuffer : DMA2D_Buffer := To_DMA2D_Buffer(Display.Hidden_Buffer(1).all);
 	begin
 		Y := (Integer(id) - 1) / GRID_WIDTH;
 
 		if Y mod 2 = 0 then
 			X := GRID_WIDTH - ((Integer(id) - 1) mod GRID_WIDTH) - 1;
-			-- X := (Integer(id) - 1) mod GRID_WIDTH;
 		else
 			X := (Integer(id) - 1) mod GRID_WIDTH;
 		end if;
 
-		Y := Y * CELL_SIZE + SPRITE_SIZE;
-		X := X * CELL_SIZE + SPRITE_SIZE;
+		Y := Y * CELL_SIZE;
+		X := X * CELL_SIZE;
+
+		STM32.DMA2D.DMA2D_Copy_Rect(
+			Src_Buffer  => ENEMY_Buffer,
+			X_Src       => 0,
+			Y_Src       => 0,
+			Dst_Buffer  => framebuffer,
+			X_Dst       => X,
+			Y_Dst       => Y,
+			Bg_Buffer   => STM32.DMA2D.Null_Buffer,
+			X_Bg        => 0,
+			Y_Bg        => 0,
+			Width       => ENEMY_Buffer.Width,
+			Height      => ENEMY_Buffer.Height,
+			Synchronous => True
+		);
 
 		-- TODO: to replace with bitmap drawing
-		Display.Hidden_Buffer(1).Set_Source(HAL.Bitmap.Red);
-		Display.Hidden_Buffer(1).Fill_Circle((X, Y), SPRITE_SIZE);
+		-- Display.Hidden_Buffer(1).Set_Source(HAL.Bitmap.Red);
+		-- Display.Hidden_Buffer(1).Fill_Circle((X, Y), SPRITE_SIZE);
 	end DrawEnemy;
 
 	procedure DrawPlayer(X, Y : in RangedPos) is
 		PY, PX : Float;
+		framebuffer : DMA2D_Buffer := To_DMA2D_Buffer(Display.Hidden_Buffer(1).all);
 	begin
 		-- project to frustrum-space coordinates
 		PX := Float(X) / Float(RangedPos'Last);
 		PY := Float(Y) / Float(RangedPos'Last);
 
 		-- transform to screen-space coordinates
-		PX := PX * Float(SCREEN_WIDTH - PLAYER_SIZE);
-		PY := PY * Float(SCREEN_HEIGHT - PLAYER_SIZE);
+		PX := PX * Float(SCREEN_WIDTH - PLAYER_SPRITE_SIZE);
+		PY := PY * Float(SCREEN_HEIGHT - PLAYER_SPRITE_SIZE);
 
-		Display.Hidden_Buffer(1).Set_Source(HAL.Bitmap.Yellow);
-		Display.Hidden_Buffer(1).Fill_Rect((
-			( Natural(PX), Natural(PY) ),
-			PLAYER_SIZE,
-			PLAYER_SIZE
-		));
+		STM32.DMA2D.DMA2D_Copy_Rect(
+			Src_Buffer  => PLAYER_Buffer,
+			X_Src       => 0,
+			Y_Src       => 0,
+			Dst_Buffer  => framebuffer,
+			X_Dst       => Natural(PX),
+			Y_Dst       => Natural(PY),
+			Bg_Buffer   => STM32.DMA2D.Null_Buffer,
+			X_Bg        => 0,
+			Y_Bg        => 0,
+			Width       => PLAYER_Buffer.Width,
+			Height      => PLAYER_Buffer.Height,
+			Synchronous => True
+		);
 	end DrawPlayer;
 
 	-- Draw a particle at the given position
@@ -86,7 +155,8 @@ package body Renderer is
 
 	procedure Flip is
 	begin
-		Display.Update_Layer (1, Copy_Back => True);
+		Display.Update_Layers;
+		-- Display.Update_Layer (1, Copy_Back => True);
 	end Flip;
 
 end Renderer;
--- src/renderer.ads
+++ src/renderer.ads
@@ -2,15 +2,81 @@ with BMP_Fonts;
 with HAL.Bitmap;            use HAL.Bitmap;
 with LCD_Std_Out;
 
-package Renderer is
+with HAL; 		    use HAL;
+with Interfaces.C; 	    use Interfaces.C;
+with STM32.DMA2D;           use STM32.DMA2D;
+with STM32.DMA2D_Bitmap;    use STM32.DMA2D_Bitmap;
+-- with STM32.Board;           use STM32.Board;
+-- with STM32.SDRAM;           use STM32.SDRAM;
+-- with System;
+
+package Renderer
+	with SPARK_Mode
+is
+	-- pragma SPARK_Mode (On);
+	-- External resources
+		-- BMP header: 18 bytes
+		-- img size: 8X8
+		-- img depth: 4 bytes / BGRA
+
+	SPRITE_SIZE : constant Natural := 8;
+	type Sprite is array(unsigned) of aliased Interfaces.Unsigned_8;
+	type Sprite_Access is not null access constant Sprite;
+
+	SPRITE_ENEMY : aliased SPRITE;
+	pragma Import (C, SPRITE_ENEMY, "sprite_enemy");
+	SPRITE_PLAYER : aliased SPRITE;
+	pragma Import (C, SPRITE_PLAYER, "sprite_player");
+
 	-- GRID SIZE: number of cells to divide the screen in.
 	GRID_HEIGHT : constant Integer := 8;
-	GRID_WIDTH : constant Integer := 7;
-	CELL_COUNT : constant Integer := GRID_WIDTH * GRID_HEIGHT;
+	GRID_WIDTH  : constant Integer := 7;
+	CELL_COUNT  : constant Integer := GRID_WIDTH * GRID_HEIGHT;
 
 	-- Display constants
-	SCREEN_WIDTH : constant Integer := 238;
+	SCREEN_WIDTH  : constant Integer := 238;
 	SCREEN_HEIGHT : constant Integer := 320;
+
+	-- ==================
+	-- DMA sprite storage
+	-- ==================
+	-- The whole game use only the colors present in this palette
+	COLOR_PALETTE : constant array(Uint8) of DMA2D_Color := (
+		(  0,   0,   0,   0),
+		(255, 255,   0,   0),
+		(255,   0, 255,   0),
+		(255,   0,   0, 255),
+		others => (0, 0, 0, 0)
+	);
+
+	-- A sprite's size is the size on the screen. Does not represent the TGA size.
+	-- Each sprite is stored in its own DMA buffer.
+	type Indexed_Bitmap is array(Natural range <>) of UInt8 with Pack;
+	BPP : constant Positive := 8;
+
+	-- The size of the displayed enemy sprite in pixels
+	ENEMY_SPRITE_SIZE : constant Positive := 32;
+	ENEMY_Indices 	  : Indexed_Bitmap(0 .. ENEMY_SPRITE_SIZE ** 2 - 1);
+	ENEMY_Buffer  	  : constant DMA2D_Buffer := (
+		Color_Mode      => L8,
+		Addr            => ENEMY_Indices(0)'Address,
+		Width           => ENEMY_SPRITE_SIZE,
+		Height          => ENEMY_SPRITE_SIZE,
+		CLUT_Color_Mode => ARGB8888,
+		CLUT_Addr       => COLOR_PALETTE(0)'Address
+	);
+
+	-- The size of the displayed player sprite in pixels
+	PLAYER_SPRITE_SIZE : constant Positive := 32;
+	PLAYER_Indices 	  : Indexed_Bitmap(0 .. PLAYER_SPRITE_SIZE ** 2 - 1);
+	PLAYER_Buffer  	  : constant DMA2D_Buffer := (
+		Color_Mode      => L8,
+		Addr            => PLAYER_Indices(0)'Address,
+		Width           => PLAYER_SPRITE_SIZE,
+		Height          => PLAYER_SPRITE_SIZE,
+		CLUT_Color_Mode => ARGB8888,
+		CLUT_Addr       => COLOR_PALETTE(0)'Address
+	);
 
 	-- Type Definition
 	type CellId is range 1 .. CELL_COUNT with Default_Value => 1;
@@ -37,16 +103,24 @@ package Renderer is
 
 	-- size of 1 cell in pixels
 	CELL_SIZE : constant Integer := SCREEN_WIDTH / GRID_WIDTH;
-
-	-- ennemy sprite size in pixels
-	SPRITE_SIZE : constant Integer := CELL_SIZE / 2;
-
-	-- player sprite size in pixels
-	PLAYER_SIZE : constant Integer := 20;
-
 	-- particle sprite size in pixels
 	PARTICLE_SIZE : constant Integer := 5;
 private
 	BACKGROUND_COLOR : constant Bitmap_Color := (Alpha => 255, others => 0);
+
+	function Sample_Sprite(
+		Img : in Sprite_Access;
+		X : in Float;
+		Y : in Float
+	) return UInt8
+		with Pre =>  X >= 0.0 and then X <= 1.0 and then
+			Y >= 0.0 and then Y <= 1.0,
+			Post => Sample_Sprite'Result >= 0 and then Sample_Sprite'Result < 1;
+
+	procedure Load_Sprite(
+		Source     : in Sprite_Access;
+		Indices    : in out Indexed_Bitmap;
+		Size 	   : in Positive);
+
 
 end Renderer;
